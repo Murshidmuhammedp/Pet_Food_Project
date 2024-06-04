0beb00a85d94d9cae228c26abe1376e3b1a081f6
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-06-04 19:48:19 +0530

    Update Login page

--- E-commerce-project/my-project/src/User/Pages/Login.jsx
+++ E-commerce-project/my-project/src/User/Pages/Login.jsx
@@ -1,14 +1,11 @@
-import React, { useContext, useState } from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
-import { Mycontext } from '../../App';
 
 function Login() {
 
     const [email, setemail] = useState("");
     const [password, setpassword] = useState("");
-    const { setcate, setfilter, filter, setsearch } = useContext(Mycontext)
-
 
     const navigate = useNavigate();
 
@@ -41,93 +38,56 @@ function Login() {
 
     return (
         <>
-             <div className="navbar bg-base-100 z-40">
-                <div className='navbar-start'>
-                    <div className='dropdown dropdown-hover'>
-                        <div tabIndex={0} className='btn btn-ghost' role='button'><h1 className='text-2xl'>Category</h1></div>
-                        <ul tabIndex={0} className='menu menu-sm dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box sm:w-52'>
-                            <li ><a className='text-lg'
-                                onClick={() => {
-                                    setcate("Cat")
-                                    navigate('/category')
-                                }}
-                            >Cat</a>
-                            </li>
-                            <li><a className='text-lg'
-                                onClick={() => {
-                                    setcate("Dog")
-                                    navigate('/category')
-                                }}
-                            >Dog</a>
-                            </li>
-                            <li><a className='text-lg'
-                                onClick={() => {
-                                    setcate("Birds")
-                                    navigate('/category')
-                                }}
-                            >Birds</a>
-                            </li>
-                            <li><a className='text-lg'
-                                onClick={() => {
-                                    setcate("fish")
-                                    navigate('/category')
-                                }}
-                            >Fish</a>
-                            </li>
-                        </ul>
+            <div>
+                <div className="navbar bg-white shadow-md p-4">
+                    <div className='flex-1'>
+                        <button onClick={() => { navigate('/') }} className='text-2xl font-serif font-bold text-blue-600'>PetPro</button>
                     </div>
-                </div>
-                <div className='text-2xl font-serif font-bold text-red-600'><button onClick={() => { navigate('/') }}>PetPro</button></div>
-                <div className="navbar-end">
-                    <div className="form-control">
-                        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto"
-                            onChange={e => setsearch(e.target.value)} onClick={() => navigate('/searchbar')} />
-                    </div>
-                    <div className='dropdown dropdown-hover'>
-                        <div tabIndex={0} className='btn btn-ghost' role='button'><h1 className='text-2xl'>User</h1></div>
-                        <ul tabIndex={0} className='menu menu-sm dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box sm:w-52'>
-                            <li><a className='text-lg'
-                                onClick={() => {
-                                    navigate('/AdminLogin')
-                                }}
-                            >Admin</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="dropdown dropdown-end">
-                    </div>
-                </div>
-            </div>
-
-            <div className="hero bg-base-100">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-
-                    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-150">
-                        <form className="card-body" onSubmit={proceedlogin}>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">E-mail</span>
-                                </label>
-                                <input type="text" placeholder="E-mail" className="input input-bordered" required onChange={e => setemail(e.target.value)} />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required onChange={e => setpassword(e.target.value)} />
-
-                            </div>
-                            <div className="form-control mt-6">
-                                <button type="submit" className="btn btn-primary">Login</button>
-                            </div>
-                        </form>
-                        <div className="mt-12">
-                            <button className="btn btn-success" onClick={() => navigate('/registrationform')}>Create new account</button>
+                    <div className="flex-none">
+                        <div className='dropdown dropdown-hover'>
+                            <button tabIndex={0} className='btn btn-ghost text-xl' role='button'>User</button>
+                            <ul tabIndex={0} className='menu menu-sm dropdown-content mt-3 z-10 p-2 shadow bg-white text-gray-700 rounded-box w-52'>
+                                <li><a className='text-lg hover:bg-gray-100'
+                                    onClick={() => {
+                                        navigate('/AdminLogin')
+                                    }}
+                                >Admin</a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Pet Foods</h1>
-                        <p className="py-6">“The best way to show your love for your pet is by feeding them nutritious and high-quality food.” </p>
+                </div>
+
+                <div className="hero min-h-screen bg-gray-100">
+                    <div className="hero-content flex-col lg:flex-row-reverse items-center lg:items-start">
+
+                        <div className="card w-full max-w-sm shadow-2xl bg-white p-6 rounded-lg">
+                            <form className="card-body" onSubmit={proceedlogin}>
+                                <div className="form-control mb-4">
+                                    <label className="label">
+                                        <span className="label-text">E-mail</span>
+                                    </label>
+                                    <input type="text" placeholder="E-mail" className="input input-bordered bg-white" required onChange={e => setemail(e.target.value)} />
+                                </div>
+                                <div className="form-control mb-4">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" placeholder="password" className="input input-bordered bg-white" required onChange={e => setpassword(e.target.value)} />
+
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button type="submit" className="btn btn-primary w-full">Login</button>
+                                </div>
+                            </form>
+                            <div className="mt-6">
+                                <button className="btn btn-secondary w-full" onClick={() => navigate('/registrationform')}>Create new account</button>
+                            </div>
+                        </div>
+                        <div className="text-center lg:text-left lg:mr-12">
+                            <h1 className="text-5xl font-bold text-blue-600">Pet Foods</h1>
+                            <p className="py-6 text-gray-700">“The best way to show your love for your pet is by feeding them nutritious and high-quality food.” </p>
+                        </div>
                     </div>
                 </div>
             </div>
